c5830dc99ce9b4fafb90c46401d8fed5c4213cba
Reynaldo <silverspecks07@gmail.com>
2024-11-24 02:07:51 -0800

    update input.go

--- pkg/cli/input.go
+++ pkg/cli/input.go
@@ -2,22 +2,35 @@ package cli
 
 import (
 	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+	"os"
+	"fmt"
 )
 
-// Read input from the user
-func ReadInput(s tcell.Screen) string {
-	event := s.PollEvent()
-	switch ev := event.(type) {
-	case *tcell.EventKey:
-		switch ev.Key() {
-		case tcell.KeyArrowUp:
-			return "up"
-		case tcell.KeyArrowDown:
-			return "down"
-		case tcell.KeyEnter:
-			return "enter"
+// ReadInput handles user key presses
+func ReadInput() string {
+	app := tview.NewApplication()
+	inputCh := make(chan string)
+
+	// set up the key listener
+	go func() {
+		for {
+			// set up input listener
+			switch ev := app.GetEvent(); ev.(type) {
+			case *tcell.EventKey:
+				key := ev.(*tcell.EventKey).Key()
+				switch key {
+				case tcell.KeyArrowUp: // for up arrow
+					inputCh <- "w"
+				case tcell.KeyArrowDown: // for down arrow
+					inputCh <- "s"
+				case tcell.KeyEnter: // for enter
+					inputCh <- "enter"
+				}
+			}
 		}
-	}
-	return ""
+	}()
+
+	return <-inputCh
 }
 
