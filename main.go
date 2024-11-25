97c6c06c6d0d6df531edb3eccad725c481ab5511
Reynaldo <silverspecks07@gmail.com>
2024-11-25 15:17:37 -0800

    update menu.go and submenu

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"log"
+
 	"github.com/rey-e-b/Owl/pkg/cli"
+	"github.com/rivo/tview"
 )
 
 func main() {
+	// create a new application
+	app := tview.NewApplication()
 
-	cli.Menu()
+	// call the main menu and pass the app instance
+	if err := cli.Menu(app); err != nil {
+		log.Fatalf("error running app: %+v", err)
+	}
 }
+
